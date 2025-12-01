16ae095fc708fc0129b028d2b2ae4e1964538f06
DeepaReddy-databeat <deepa.baini@databeat.io>
2025-12-01 23:47:33 +0530

    your message

--- models/example/employees_tansformed.sql
+++ models/example/employees_tansformed.sql
@@ -1,9 +1,9 @@
 {{ config(materialized='table') }}
 
 select
-    EmployeeID,
-    FirstName,
-    LastName,
-    upper(Department) as Department,
+    EmployeeID as employee_id,
+    FirstName as first_name,
+    LastName as last_name,
+    upper(Department)as department,
     datediff(year, JoinDate, getdate()) as YearsAtCompany
 from {{ ref('employee') }}
